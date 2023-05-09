a1f0f96002d5cf0de33451fa2bd125f3989985de
deletella01 <deletella01@gmail.com>
2023-05-09 21:30:14 +0000

    Task 4 correction6

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -37,12 +37,12 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     for c in results.get("children"):
         title = c.get("data").get("title").lower().split()
         for word in word_list:
-             if word.lower() in title:
-                 times = len([t for t in title if t == word.lower()])
-                 if instances.get(word) is None:
-                     instances[word] = times
-                 else:
-                     instances[word] += times
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
 
     if after is None:
         if len(instances) == 0:

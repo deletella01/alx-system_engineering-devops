82a83a49fb6a98e323678190347cb03238778c5b
deletella01 <deletella01@gmail.com>
2023-05-09 20:26:11 +0000

    Task 0 correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,5 +13,5 @@ def number_of_subscribers(subreddit):
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
             headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-            subs = r.get("data", {}).get("subscribers", 0)
-            return subs
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
